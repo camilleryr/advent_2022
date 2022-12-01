31460f0c4b6ca2f6c044151ac74423935bab8505
LEX5591 <32465602+camilleryr@users.noreply.github.com>
2022-12-01 06:41:31 -0600

    Day 1

---
+++ README.md
@@ -0,0 +1,13 @@
+****
+
+Advent Of Code 2022 Execution Times (in ms)
+
+Puzzles can be found [here](https://adventofcode.com/2022/)
+
+----
+
+| Day | Part | Execution Time |
+| --- | ---- | -------------- |
+| 1 | 1 | 8 ms|
+| 1 | 2 | 1 ms|
+||total|9 ms|
---
+++ lib/mix/tasks/gen_puzzle.ex
@@ -0,0 +1,84 @@
+defmodule Mix.Tasks.GenPuzzle do
+  @moduledoc """
+    `mix gne_puzzle 1`
+  """
+
+  use Mix.Task
+
+  @shortdoc "Solve a problem by day and part"
+  def run([day]) do
+    :inets.start()
+    :ssl.start()
+
+    gen_input_file(day)
+    gen_puzzle_file(day)
+  end
+
+  defp gen_puzzle_file(day) do
+    File.write("./lib/puzzles/day_#{day}.ex", puzzle_template(day))
+  end
+
+  defp gen_input_file(day) do
+    input = get("input", day)
+
+    File.write("./input/day_#{day}.txt", input)
+  end
+
+  defp get(path \\ "", day) do
+    cookie = Application.get_env(:elixir, :aoc_cookie)
+    base = "https://adventofcode.com/2022/day/#{day}"
+
+    url =
+      case path do
+        "" -> base
+        path -> "#{base}/#{path}"
+      end
+      |> to_charlist()
+
+    {:ok, {_req, _headers, results}} =
+      :httpc.request(:get, {url, [cookie]}, [verify: :verify_none], verify: :verify_none)
+
+    results
+  end
+
+  defp puzzle_template(day) do
+    puzzle = day |> get() |> parse()
+
+    ~s"""
+    defmodule Day#{day} do
+      import Advent2022
+
+      @doc ~S\"\"\"
+      #{puzzle}
+
+      ## Example
+
+        iex> part_1()
+      \"\"\"
+      def_solution part_1(stream_input) do
+      end
+    end
+    """
+    |> String.replace("\n\n", "\n")
+    |> String.trim("\n")
+    |> Code.format_string!()
+  end
+
+  defp parse(htlm_as_charlist) do
+    {:safe, result} =
+      htlm_as_charlist
+      |> to_string()
+      |> String.split(~r/<\/?article[a-z0-9\s\\"=-]*>/)
+      |> Enum.drop(1)
+      |> List.first()
+      |> String.replace(~r/<li>/, "\\g{1}- ", global: true)
+      |> String.replace(
+        ~r/<\/?\s?br>|<\/\s?p>|<\/\s?div>|<\/\s?h.>/,
+        "\\g{1}\n",
+        global: true
+      )
+      |> PhoenixHtmlSanitizer.Helpers.sanitize(:strip_tags)
+
+    result
+  end
+end
